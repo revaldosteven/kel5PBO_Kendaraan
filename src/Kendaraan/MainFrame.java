e2e695558896071345745cd88eead79ec20728bf
revaldosteven <revaldosteven1986@gmail.com>
2024-10-03 18:39:56 +0700

    Update Data Frame

--- src/Kendaraan/MainFrame.java
+++ src/Kendaraan/MainFrame.java
@@ -186,17 +186,17 @@ public class MainFrame extends javax.swing.JFrame {
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
         // TODO add your handling code here:
-       // new KeretaapiFrame().setVisible(true);
+       new KeretapiFrame().setVisible(true);
     }//GEN-LAST:event_jButton2ActionPerformed
 
     private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
         // TODO add your handling code here:
-        //new PesawatFrame().setVisible(true);
+        new PesawatFrame().setVisible(true);
     }//GEN-LAST:event_jButton3ActionPerformed
 
     private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
         // TODO add your handling code here:
-        //new TrukFrame().setVisible(true);
+        new TrukFrame().setVisible(true);
     }//GEN-LAST:event_jButton4ActionPerformed
 
     private void jButton5ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton5ActionPerformed
