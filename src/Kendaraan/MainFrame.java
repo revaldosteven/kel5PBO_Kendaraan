49cc9ef96685bd49b7bb8e48addf2b57e760b653
revaldosteven <revaldosteven1986@gmail.com>
2024-10-05 07:25:11 +0700

    Update Data Frame

--- src/Kendaraan/MainFrame.java
+++ src/Kendaraan/MainFrame.java
@@ -206,7 +206,7 @@ public class MainFrame extends javax.swing.JFrame {
 
     private void jButton6ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton6ActionPerformed
         // TODO add your handling code here:
-        //new KapalFrame().setVisible(true);
+        new KapalFrame().setVisible(true);
     }//GEN-LAST:event_jButton6ActionPerformed
 
     private void jButton7ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton7ActionPerformed
