89dfa016943de7ce2d855a422c6f9def5478e4d6
nadiatur <rodiyahwifna@gmail.com>
2024-10-14 14:44:12 +0700

    Update Data Inheritance Wifna

--- src/Kendaraan/Wifna_Kapal.java
+++ src/Kendaraan/Wifna_Kapal.java
@@ -8,47 +8,15 @@ package Kendaraan;
  * 
  * @author USER
  */
-public class Wifna_Kapal {
-    String tipe,kapasitas_penumpang,bahan_bakar,kecepatan_maksimal,fitur;
-    
-    public void settipe(String tipe){
-        this.tipe = tipe;
+public class Wifna_Kapal extends kendaraan{
+    String jenis_lambung;
+
+    public String getJenis_lambung() {
+        return jenis_lambung;
     }
-    
-    public String gettipe(){
-        return tipe;
+
+    public void setJenis_lambung(String jenis_lambung) {
+        this.jenis_lambung = jenis_lambung;
     }
-    
-    public void setkapasitas_penumpang(String kapasitas_penumpang){
-        this.kapasitas_penumpang = kapasitas_penumpang;
-    }
-    
-    public String getkapasitas_penumpang(){
-        return kapasitas_penumpang;
-    }
-    
-    public void setbahan_bakar(String bahan_bakar){
-        this.bahan_bakar = bahan_bakar;
-    }
-    
-    public String getbahan_bakar(){
-        return bahan_bakar;
-    }
-    
-    public void setkecepatan_maksimal(String kecepatan_maksimal){
-        this.kecepatan_maksimal = kecepatan_maksimal;
-        
-    }
-    
-    public String getkecepatan_maksimal(){
-        return kecepatan_maksimal;
-    }
-    
-    public void setfitur(String fitur){
-        this.fitur = fitur;
-    }
-    
-    public String getfitur(){
-        return fitur;
-    }
+ 
 }
