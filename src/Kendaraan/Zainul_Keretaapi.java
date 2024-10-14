181fc3f88c733c030366472b897a07bfd1b773f0
zainularifi <akunarifin98@gmail.com>
2024-10-14 14:22:33 +0700

    Update Data Inheritance Zainul

--- src/Kendaraan/Zainul_Keretaapi.java
+++ src/Kendaraan/Zainul_Keretaapi.java
@@ -8,47 +8,15 @@ package Kendaraan;
  *
  * @author zainu
  */
-public class Zainul_Keretaapi {
-    String tipe,kapasitas_penumpang,bahan_bakar,kecepatan_maksimal,fitur;
-    
-    public void settipe(String tipe){
-        this.tipe = tipe;
+public class Zainul_Keretaapi extends kendaraan{
+    String tipe_jalur;
+
+    public String getTipe_jalur() {
+        return tipe_jalur;
+    }
+
+    public void setTipe_jalur(String tipe_jalur) {
+        this.tipe_jalur = tipe_jalur;
     }
     
-    public String gettipe(){
-        return tipe;
-    }
-    
-    public void setkapasitas_penumpang(String kapasitas_penumpang){
-        this.kapasitas_penumpang = kapasitas_penumpang;
-    }
-    
-    public String getkapasitas_penumpang(){
-        return kapasitas_penumpang;
-    }
-    
-    public void setbahan_bakar(String bahan_bakar){
-        this.bahan_bakar = bahan_bakar;
-    }
-    
-    public String getbahan_bakar(){
-        return bahan_bakar;
-    }
-    
-    public void setkecepatan_maksimal(String kecepatan_maksimal){
-        this.kecepatan_maksimal = kecepatan_maksimal;
-        
-    }
-    
-    public String getkecepatan_maksimal(){
-        return kecepatan_maksimal;
-    }
-    
-    public void setfitur(String fitur){
-        this.fitur = fitur;
-    }
-    
-    public String getfitur(){
-        return fitur;
-    }
 }
