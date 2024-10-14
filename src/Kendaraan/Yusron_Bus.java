f2235e901bea8881f0b2663bda0dda8b58cddeca
yusron1234 <yusronkafabi0@gmail.com>
2024-10-14 13:51:06 +0700

    Update Data Inheritance Yusron

--- src/Kendaraan/Yusron_Bus.java
+++ src/Kendaraan/Yusron_Bus.java
@@ -9,46 +9,14 @@ package Kendaraan;
  * @author Admin
  */
 public class Yusron_Bus {
-    String tipe,kapasitas_penumpang,bahan_bakar,kecepatan_maksimal,fitur;
-    
-    public void settipe(String tipe){
-        this.tipe = tipe;
+    String kelas_penumpang;
+
+    public String getKelas_penumpang() {
+        return kelas_penumpang;
+    }
+
+    public void setKelas_penumpang(String kelas_penumpang) {
+        this.kelas_penumpang = kelas_penumpang;
     }
     
-    public String gettipe(){
-        return tipe;
-    }
-    
-    public void setkapasitas_penumpang(String kapasitas_penumpang){
-        this.kapasitas_penumpang = kapasitas_penumpang;
-    }
-    
-    public String getkapasitas_penumpang(){
-        return kapasitas_penumpang;
-    }
-    
-    public void setbahan_bakar(String bahan_bakar){
-        this.bahan_bakar = bahan_bakar;
-    }
-    
-    public String getbahan_bakar(){
-        return bahan_bakar;
-    }
-    
-    public void setkecepatan_maksimal(String kecepatan_maksimal){
-        this.kecepatan_maksimal = kecepatan_maksimal;
-        
-    }
-    
-    public String getkecepatan_maksimal(){
-        return kecepatan_maksimal;
-    }
-    
-    public void setfitur(String fitur){
-        this.fitur = fitur;
-    }
-    
-    public String getfitur(){
-        return fitur;
-    }
 }
