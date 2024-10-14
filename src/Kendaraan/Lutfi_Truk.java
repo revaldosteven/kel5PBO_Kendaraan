236b49a8980314ca4b628e9508429ae3a73099a0
lutfirahman053 <rahmanrahman8063@gmail.com>
2024-10-14 15:01:08 +0700

    Update Data Inheritance Lutfi

--- src/Kendaraan/Lutfi_Truk.java
+++ src/Kendaraan/Lutfi_Truk.java
@@ -8,47 +8,15 @@ package Kendaraan;
  *
  * @author hp
  */
-public class Lutfi_Truk {
-    String tipe,kapasitas_penumpang,bahan_bakar,kecepatan_maksimal,fitur;
-    
-    public void settipe(String tipe){
-        this.tipe = tipe;
+public class Lutfi_Truk extends kendaraan {
+    String tipe_muatan;
+
+    public String getTipe_muatan() {
+        return tipe_muatan;
+    }
+
+    public void setTipe_muatan(String tipe_muatan) {
+        this.tipe_muatan = tipe_muatan;
     }
     
-    public String gettipe(){
-        return tipe;
-    }
-    
-    public void setkapasitas_penumpang(String kapasitas_penumpang){
-        this.kapasitas_penumpang = kapasitas_penumpang;
-    }
-    
-    public String getkapasitas_penumpang(){
-        return kapasitas_penumpang;
-    }
-    
-    public void setbahan_bakar(String bahan_bakar){
-        this.bahan_bakar = bahan_bakar;
-    }
-    
-    public String getbahan_bakar(){
-        return bahan_bakar;
-    }
-    
-    public void setkecepatan_maksimal(String kecepatan_maksimal){
-        this.kecepatan_maksimal = kecepatan_maksimal;
-        
-    }
-    
-    public String getkecepatan_maksimal(){
-        return kecepatan_maksimal;
-    }
-    
-    public void setfitur(String fitur){
-        this.fitur = fitur;
-    }
-    
-    public String getfitur(){
-        return fitur;
-    }
 }
